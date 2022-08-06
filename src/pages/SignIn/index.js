9ad38a30f232d7d1368edd58b245de20becd1afe
emily <nakanodepaula@gmail.com>
2022-08-06 19:31:01 -0300

    send login to the API and receive token

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,14 +2,25 @@ import HeaderDisconnected from "../../components/HeaderDisconnected.js"
 import Logo from "../../components/Logo.js"
 import { Container } from "../SignUp/style.js"
 
-import { useState } from "react"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function SignIn() {
+    const navigate = useNavigate()
     const [user, setUser] = useState({
         email: '',
         password: ''
     })
+    const [data, setData] = useState('')
     async function HandleSubmit(e) {
         e.preventDefault()
+        try {
+            const res = await axios.post("https://project-back-shortly.herokuapp.com/sign-in", {...user})
+            console.log(res.data)
+            navigate('/')
+        } catch {
+            alert("Preencha os dados corretamente")
+        }
     }
     function ChangeInput(e) {
         setUser({...user, [e.target.name]: e.target.value})
