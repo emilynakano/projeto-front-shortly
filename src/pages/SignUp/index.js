d99278851d7fff403398a015a696d8d9689cd962
emily <nakanodepaula@gmail.com>
2022-08-06 17:14:06 -0300

    sign-up page layout

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,12 +2,19 @@ import { Container } from "./style.js";
 
 import HeaderDisconnected from "../../components/HeaderDisconnected.js";
 import Logo from '../../components/Logo.js'
+
 export default function SignUp() {
     return (
         <>
             <HeaderDisconnected />
             <Container>
                 <Logo />
+                <form>
+                    <input type="text" placeholder="nome" />
+                    <input type="text" placeholder="email"  />
+                    <input type="password" placeholder="senha" />
+                    <input type="password" placeholder="confirme a senha" />
+                </form>
             </Container>
         </>
     )
--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 const Container = styled.div `
-    
+    display:flex;
+    flex-direction: column;
+    align-items:center;
+
+    gap:50px;
+
+    width: 100%;
+    form {
+        display:flex;
+        flex-direction: column;
+        gap: 10px;
+        width: 70%
+    }
+    input {
+        height: 60px;
+
+        border: 1px solid rgba(120, 177, 89, 0.25);
+        box-shadow: 0px 4px 24px rgba(120, 177, 89, 0.12);
+        border-radius: 12px;
+
+        padding:20px;
+    }
 `;
 export {Container}
