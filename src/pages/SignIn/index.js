78a1b350427285eed3dcc7954f2c942830d564bb
emily <nakanodepaula@gmail.com>
2022-08-06 20:22:13 -0300

    insert token in UserContext

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -5,8 +5,10 @@ import { Container } from "../SignUp/style.js"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext.js";
 export default function SignIn() {
-    
+    const {user, setUser} = useContext(UserContext)
     const navigate = useNavigate()
     const [userLogin, setUserLogin] = useState({
         email: '',
@@ -16,6 +18,7 @@ export default function SignIn() {
         e.preventDefault()
         try {
             const res = await axios.post("https://project-back-shortly.herokuapp.com/sign-in", {...userLogin})
+            setUser({...user, token: res.data})
             navigate('/')
         } catch {
             alert("Preencha os dados corretamente")
