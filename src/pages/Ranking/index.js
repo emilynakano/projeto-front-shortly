d52733b47fe199d2454d90cdda2cf9ddeb8afca3
emily <nakanodepaula@gmail.com>
2022-08-07 14:20:17 -0300

    use of ternary to verification text above

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -6,13 +6,16 @@ import Logo from "../../components/Logo.js";
 import Trophy from '../../assets/Vector.png';
 import { useEffect, useState } from "react";
 import axios from "axios";
+import UserContext from "../../contexts/UserContext.js";
+import { useContext } from "react";
 
 export default function Ranking () {
-    const [users, setUsers] = useState([])
+    const {user} = useContext(UserContext)
+    const [usersRanking, setUsersRanking] = useState([])
 
     useEffect(()=> {
         const promise = axios.get('https://project-back-shortly.herokuapp.com/ranking');
-        promise.then((res) => setUsers(res.data))
+        promise.then((res) => setUsersRanking(res.data))
     }, [])
     
     return (
@@ -25,10 +28,12 @@ export default function Ranking () {
                     <h1>Ranking</h1>
                 </div>
                 <div className="classification">
-                    {users.map((user, index)=> <h1>{index + 1}. {user.name} - {user.linksCount} links - {user.visitCount} visualizações</h1>)}
+                    {usersRanking.map((user, index)=> <h1>{index + 1}. {user.name} - {user.linksCount} links - {user.visitCount} visualizações</h1>)}
                 </div>
+                {user.token ? <></> :
                 <h2>Crie sua conta para usar nosso serviço!</h2>
-            </Container>
+                }
+                </Container>
         </>
     
     )
